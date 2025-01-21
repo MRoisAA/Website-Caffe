0cf5fa2d824cf201f98d6b8da0b4c9734aa1afe5
muhammad rois <118717474+MRoisAA@users.noreply.github.com>
2025-01-21 09:25:28 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// toggleclass active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
